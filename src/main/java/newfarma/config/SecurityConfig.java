792b7ae3602c093ae52e9b376a0615bb40c13cbf
jose <josevalentin250890@gmail.com>
2026-02-26 11:54:33 -0500

    addHealthOk

--- src/main/java/newfarma/config/SecurityConfig.java
+++ src/main/java/newfarma/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req->
-                        req.requestMatchers("/api/auth/**", "/api/cargo/**")
+                        req.requestMatchers("/api/auth/**", "/api/cargo/**","/api/check/**", "/")
                                 .permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/producto")
                                 .permitAll()
--- src/main/java/newfarma/controller/HealthCheckController.java
+++ src/main/java/newfarma/controller/HealthCheckController.java
@@ -1,9 +1,11 @@
-package controller;
+package newfarma.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/check")
 public class HealthCheckController {
 
     @GetMapping("/health")
